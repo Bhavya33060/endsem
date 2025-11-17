94865296766cc085bd34006b81206454a9e194fb
bhavya33060 <yarlagaddabhavya27@gmail.com>
2025-11-17 08:25:29 +0530

    repo link fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  base: '/endsem/',   // REQUIRED for GitHub Pages
+  base: '/endsem/',
   plugins: [react()],
 })
